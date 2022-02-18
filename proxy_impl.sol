90fa6c497ad3ed6ddff87c81bd622b17eb7dca25
Greapi <1132585563@qq.com>
2022-02-18 14:16:02 +0800

    实现代理合约: ⑴ 自动新增函数 ⑵ 自动调用函数

--- proxy_impl.sol
+++ proxy_impl.sol
@@ -1,21 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.0;
 
 contract Impl {
-    function hello() public pure returns(bytes memory) {
-        return "hello world";
+    function sqrt(uint y) public pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+        // else z = 0 (default value)
     }
 }
 
 contract Proxy {
-    address implLibrary;
+    address public implLibrary;
 
     constructor (address impl) {
         implLibrary = impl;
     }
 
-    function exec (bytes memory funsig) public returns (bool, bytes memory) {
-        (bool success, bytes memory data) = implLibrary.delegatecall(funsig);
+    function exec (bytes memory input_data) public returns (bool, bytes memory) {
+        (bool success, bytes memory data) = implLibrary.delegatecall(input_data);
         return (success, data);
     }
 
